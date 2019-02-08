93c81ef99d15f183aae05364b5eb9954aa85b176
Lucia Prieto Gonzalez <luciaprietogonzalez@MacBook-Pro-de-Lucia.local>
2019-02-08 19:13:14 +0100

    He añadido los dos ejercicios en el mismo proyecto

---
+++ ejercicio1/src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+        for(int i=0; i<= 100; i++){
+            System.out.println(i);
+
+        }
+
+        double x = 20;
+        double iva= x * 0.21;
+        System.out.println(iva);
+    }
+}
