77ee6bd04f32a70ae07ce8bfa2bf98873ceb29cb
Polamen <pdj_98rhotmail.com>
2019-02-08 19:12:27 +0100

    Ejercicio 1

--- Ejercicios/src/Main.java
+++ Ejercicios/src/Main.java
@@ -2,6 +2,12 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        int j;
+
+        for (int i = 1; i < 101; i++) {
+            j=i;
+            System.out.println(j);
+        }
+
+        }
     }
-    
-}
