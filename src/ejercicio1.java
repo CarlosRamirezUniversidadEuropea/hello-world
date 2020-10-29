5684a8d53c126fa90715e5666878678aa4a98001
AnhellOn <angelnxt@rocaberti.com>
2020-10-29 22:31:39 +0100

    Nuevo ejercicio 1

--- src/ejercicio1.java
+++ src/ejercicio1.java
@@ -1,6 +1,13 @@
 public class ejercicio1 {
+
     public static void main(String[] args) {
-        for(int i=1;i<=100;i++)
+        int[] my_array = {1,2,3,4,5};
+        String[] num_array = {"","primer","segundo","tercero","cuarto","quinto"};
+
+        for(int i : my_array) {
+            System.out.printf("Hola %s intento \n",num_array[i]);
             System.out.println(i +",");
+        }
     }
+
 }
